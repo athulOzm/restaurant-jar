1537f0dd8594540525c51dface75e9a9694a4159
unknown <mctathul@gmail.com>
2021-04-10 12:34:14 +0400

    passport integrated

--- routes/api.php
+++ routes/api.php
@@ -28,18 +28,18 @@ use App\Http\Middleware\Cors;
 //admin.
 Route::middleware([Cors::class, 'auth:userapi'])->group(function () {
     
-    Route::post('/makeurl', 'CardController@makeUrl');
-    Route::post('/addcard', 'CardController@add');
-    Route::get('/fetchaddcard', 'CardController@fetchAddCard');
-    Route::get('/fetchcards', 'CardController@all');
-    Route::get("/fetchtype/{card}", 'CardController@type');
-    Route::post('/card/upddp', 'CardController@upddp');
+    // Route::post('/makeurl', 'CardController@makeUrl');
+    // Route::post('/addcard', 'CardController@add');
+    // Route::get('/fetchaddcard', 'CardController@fetchAddCard');
+    // Route::get('/fetchcards', 'CardController@all');
+    // Route::get("/fetchtype/{card}", 'CardController@type');
+    // Route::post('/card/upddp', 'CardController@upddp');
 
-    Route::get("/fetchcontact/{card}", 'ContactController@contact');
-    Route::post('/card/updcontact', 'ContactController@updcontact');
+    // Route::get("/fetchcontact/{card}", 'ContactController@contact');
+    // Route::post('/card/updcontact', 'ContactController@updcontact');
     
-    Route::get("/fetchsocialmedia/{card}", 'SocialmediaController@socialmedia');
-    Route::post('/card/updsocialmedia', 'SocialmediaController@updsocialmedia');
+    // Route::get("/fetchsocialmedia/{card}", 'SocialmediaController@socialmedia');
+    // Route::post('/card/updsocialmedia', 'SocialmediaController@updsocialmedia');
     
     
 });
@@ -52,13 +52,13 @@ Route::middleware([Cors::class])->group(function () {
     Route::post('/userlogin', 'Auth\LoginController@userlogin');
 
     //social login
-    Route::post('/googleauthreg', 'Auth\GoogleController@register');
-    Route::post('/googleauth', 'Auth\GoogleController@auth');
+    // Route::post('/googleauthreg', 'Auth\GoogleController@register');
+    // Route::post('/googleauth', 'Auth\GoogleController@auth');
 
-    Route::post('/fbauthreg', 'Auth\FbController@register');
-    Route::post('/fbauth', 'Auth\FbController@auth');
+    // Route::post('/fbauthreg', 'Auth\FbController@register');
+    // Route::post('/fbauth', 'Auth\FbController@auth');
 
-    Route::get('/card/{card}', 'CardController@show');
+    // Route::get('/card/{card}', 'CardController@show');
 
     
 
