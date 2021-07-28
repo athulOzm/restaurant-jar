e2c4222139af8ec486e538252013ff8977c5678d
Athul M <athulm@macAthul.local>
2021-07-28 21:30:39 +0400

    api upds2

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -442,7 +442,7 @@ class ProductController extends Controller
                 'menu_type' => $mt,
                 'menus' => $cmt->products,
                 'categories' => $cmt->categories(),
-                'branches'  =>  $request->user()->branches
+                //'branches'  =>  $request->user()->branches
                 ]
         ], 201);
     }
